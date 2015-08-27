a9eb800e4fd6468b0b990d4ee56dd9eca00c36fc
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-27 13:13:40 +0200

    ups!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user! except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order("created_at DESC")
