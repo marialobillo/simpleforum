2286582942329d863cef450a4b3ec7ba36b3836a
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-29 13:14:17 +0200

    add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:comment))
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
 end
